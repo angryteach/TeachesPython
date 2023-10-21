e6db05643808ff76e9233f214869ff77f680c18f
Angry Teach <angryteachofficial@gmail.com>
2023-10-21 23:54:07 +0300

    PROJ 001 v0.1 - RIGHT TRIANGLE - Added the first simple version of the programme.

---
+++ proj-001/main.py
@@ -0,0 +1,24 @@
+#
+# This programme computes the area of a right-angled triangle
+#
+
+def main():
+    print("Let's calculate the area of a right triangle.")
+    calc()
+
+
+def calc():
+    # Get user input
+    a = float(input("What's the length of one side? "))
+    b = float(input("What's the length of the other side? "))
+
+    # calculations
+    area = 0.5 * a * b
+
+    # the output
+    print("The area is ", s)
+
+
+if __name__ == "__main__":
+    main()
+
