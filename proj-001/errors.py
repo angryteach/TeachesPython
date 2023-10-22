a37f8c6719c2324a9552dd3258092be4ffeca299
Angry Teach <angryteachofficial@gmail.com>
2023-10-22 14:03:04 +0300

    Corrected errors.py in proj-1.

--- proj-001/errors.py
+++ proj-001/errors.py
@@ -7,7 +7,7 @@ def output():
     name = input ("What's your name? ")
     print("Hello, " + name)
     age = inpout("What's your age? ")
-    print("You are %s years of age." + age)
+    print("You are " + age + " years of age.")
 
 if __name__ = "__main__":
     main()
