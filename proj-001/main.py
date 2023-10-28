6fae15741b3095121c5f31fc9513924afa7bc247
Angry Teach <angryteachofficial@gmail.com>
2023-10-28 23:26:39 +0300

    PROJ 001 v0.2 - RIGHT TRIANGLE WITHOUT ERRORS - Added try-except blocks to catch possible user errors.

--- proj-001/main.py
+++ proj-001/main.py
@@ -2,22 +2,38 @@
 # This programme computes the area of a right-angled triangle
 #
 
+# Get user input
+print("Let's calculate the area of a right triangle.")
+a = input("What's the length of one side? ")
+b = input("What's the length of the other side? ")
+
 # the entry point
 def main():
-    print("Let's calculate the area of a right triangle.")
+    check_user_input()
     calc()
 
+# Check whether user gives numbers
+def check_user_input():
+    try:
+        global a
+        global b
+        a = float(a)
+        b = float(b)
+    except ValueError:
+        print("!!!One or more numbers you gave me were not numbers!!!")
 
+
+# Do the calculation
 def calc():
-    # Get user input
-    a = float(input("What's the length of one side? "))
-    b = float(input("What's the length of the other side? "))
+    try:
+        global a
+        global b
+        area = 0.5 * a * b
+        print("The area is ", area)
+    except TypeError:
+        print("!!!Nothing to calculate. An error must have occured.!!!")
 
-    # calculations
-    area = 0.5 * a * b
 
-    # the output
-    print("The area is ", s)
 
 
 if __name__ == "__main__":
